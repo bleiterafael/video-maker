c4b17e62f9c935962dbf195f35ca750f950bfcca
bleiterafael <rafael.leite@phidelis.com.br>
2022-04-20 00:35:25 -0300

    feat: creating image robot. add google images search

--- node/index.js
+++ node/index.js
@@ -3,15 +3,17 @@ const robots = {
     userInput: require('./robots/user-input.js'),
     text: require('./robots/text.js'),
     wikipedia: require('./robots/wikipedia.js'),
-    state: require('./robots/state.js')
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js')
 }
 
 async function start(){
     
 
-    await robots.userInput()
-    await robots.wikipedia()
-    await robots.text()
+    // await robots.userInput()
+    // await robots.wikipedia()
+    // await robots.text()
+    await robots.image()
 
     const content = robots.state.load()
     console.log(content)
